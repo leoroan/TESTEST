72b294ff43d3dffff87760fece54f37af514b4bf
Leandro Maselli <leoroan@gmail.com>
2024-01-23 11:51:13 -0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { sequelize } from "./connection.js";
 import { __dirname } from "./utils.js";
 // import Entidad from "./routes/Entidad.router.js";
 import documentsRouter from "./routes/document.router.js";
@@ -25,18 +24,7 @@ app.use(express.static(__dirname + "/public"));
 // app.use("/api/entity", Entidad);
 app.use("/api/docs", documentsRouter);
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection success');
-    return sequelize.sync();
-  })
-  .then(() => {
-    console.log('Sync models');
-    app.listen(8080, () => {
-      console.log(`Server listen on http://localhost:${8080}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Connection fail', error);
-  });
+
+app.listen(8080, () => {
+  console.log(`Server listen on http://localhost:${8080}`);
+});
