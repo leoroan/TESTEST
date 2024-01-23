443994dd30eed9659372f56e1fbcb70fb6a476c6
Leandro Maselli <leoroan@gmail.com>
2024-01-23 13:17:05 -0300

    allowed cors

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 // app.use(express.json());
 
 // const corsOptions = {
