f68e8b8e695a59a6f56f6f09d1589f1b6d26efdb
Leandro R. M <77900416+leoroan@users.noreply.github.com>
2024-01-25 13:54:43 -0300

    Update index.js
    corsss

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
